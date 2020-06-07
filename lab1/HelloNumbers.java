09cad5611fd0d0e77ed005c43c009ea8dcb57257
Kaiqi Bian <jennylvy9@gmail.com>
2020-06-06 23:14:53 -0400

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,19 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int sum = 0;
+		while (x<10) {
+			sum = sum+x;
+			System.out.print(sum + " ");
+			x = x+1;
+		}
+
+	}
+}
+
+/*
+1. Before java variables can be used, they must be declared.
+2. Java variables must have a specific type.
+3. Java variable types can never change.
+*/
+
