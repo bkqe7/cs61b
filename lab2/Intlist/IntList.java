fe042f40654b1e7fc046264e97792538bc99d98d
Kaiqi Bian <jennylvy9@gmail.com>
2020-06-19 23:15:55 -0400

    completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList dummy = A;
+        while (A.rest != null) {
+            A = A.rest;
+        }
+        A.rest = B;
+        return dummy;
     }
 
     /**
@@ -91,7 +96,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A ==null) {
+            return B;
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
